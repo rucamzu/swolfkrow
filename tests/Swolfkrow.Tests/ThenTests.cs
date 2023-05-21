99085022348197a4da0f726384ac741b27306541
Rubén Campos Zurriaga <ruben.campos.zurriaga@gmail.com>
2023-05-21 23:20:06 +1000

    feat: chain workflow from factory function with one argument

--- tests/Swolfkrow.Tests/ThenTests.cs
+++ tests/Swolfkrow.Tests/ThenTests.cs
@@ -6,7 +6,7 @@ namespace Swolfkrow.Tests;
 public class ThenTests
 {
     [Test]
-    public async Task ChainedExistingWorkflowsYieldAllEventsFromBothWorkflows()
+    public async Task ChainedExistingWorkflowsYieldsAllEventsFromBothWorkflows()
     {
         var expectedEvents = Some.Events(howMany: 4).ToList();
 
@@ -26,6 +26,22 @@ public class ThenTests
         var actualEvents = await Workflow
             .Start(() => Some.Workflow.FromEvents(expectedEvents.Take(2)))
             .Then(() => Some.Workflow.FromEvents(expectedEvents.Skip(2).Take(2)))
+            .ToListAsync();
+
+        actualEvents.Should().Equal(expectedEvents);
+    }
+
+
+    [Test]
+    public async Task ChainedWorkflowFactoryWithArgumentYieldsAllEventsFromBothWorkflows()
+    {
+        var expectedEvents = Some.Events(howMany: 4).ToList();
+
+        var actualEvents = await Workflow
+            .Start(Some.Workflow.FromEvents(expectedEvents.Take(2)))
+            .Then(
+                createContinuation: (int n) => Some.Workflow.FromEvents(expectedEvents.Skip(2).Take(n)),
+                arg: 2)
             .ToListAsync();
 
         actualEvents.Should().Equal(expectedEvents);
