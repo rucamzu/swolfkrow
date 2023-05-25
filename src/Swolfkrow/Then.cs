a3368a3cea02f3a4c3cf9e54e551d1dd473b9dd6
Rubén Campos Zurriaga <ruben.campos.zurriaga@gmail.com>
2023-05-25 10:28:45 +1000

    feat: continue workflow from factory function with three arguments

--- src/Swolfkrow/Then.cs
+++ src/Swolfkrow/Then.cs
@@ -54,4 +54,21 @@ public static partial class Workflow
     public static IAsyncEnumerable<TEvent> Then<TArg1, TArg2, TEvent>(this IAsyncEnumerable<TEvent> workflow,
         Func<TArg1, TArg2, IAsyncEnumerable<TEvent>> createContinuation, TArg1 arg1, TArg2 arg2)
         => workflow.Concat(Workflow.Start(createContinuation, arg1, arg2));
+
+    /// <summary>
+    /// Continues an existing asynchronous workflow with the asynchronous workflow created by a factory function that takes three arguments. 
+    /// </summary>
+    /// <param name="workflow">An existing asynchronous workflow.</param>
+    /// <param name="createContinuation">A factory function that takes three arguments and returns an asynchronous workflow that is to be executed as continuation to the given <paramref name="workflow"/>.</param>
+    /// <param name="arg1">The first argument passed to the given <paramref name="createContinuation"/> factory function.</param>
+    /// <param name="arg2">The second argument passed to the given <paramref name="createContinuation"/> factory function.</param>
+    /// <param name="arg3">The third argument passed to the given <paramref name="createContinuation"/> factory function.</param>
+    /// <typeparam name="TArg1">The type of the first argument passed to the given <paramref name="createContinuation"/> factory function.</typeparam>
+    /// <typeparam name="TArg2">The type of the second argument passed to the given <paramref name="createContinuation"/> factory function.</typeparam>
+    /// <typeparam name="TArg3">The type of the third argument passed to the given <paramref name="createContinuation"/> factory function.</typeparam>
+    /// <typeparam name="TEvent">The (base) type of the events yielded by both chained asynchronous workflows.</typeparam>
+    /// <returns>An asynchronous workflow composed of the given <paramref name="workflow"/>, followed by an asynchronous workflow that calls the given <paramref name="createContinuation"/> factory function with the given arguments and yields the events of the resulting asynchronous workflow.</returns>
+    public static IAsyncEnumerable<TEvent> Then<TArg1, TArg2, TArg3, TEvent>(this IAsyncEnumerable<TEvent> workflow,
+        Func<TArg1, TArg2, TArg3, IAsyncEnumerable<TEvent>> createContinuation, TArg1 arg1, TArg2 arg2, TArg3 arg3)
+        => workflow.Concat(Workflow.Start(createContinuation, arg1, arg2, arg3));
 }
--- tests/Swolfkrow.Tests/ThenTests.cs
+++ tests/Swolfkrow.Tests/ThenTests.cs
@@ -61,4 +61,21 @@ public class ThenTests
 
         actualEvents.Should().Equal(expectedEvents);
     }
+
+    [Test]
+    public async Task ChainedWorkflowFactoryWithThreeArgumentsYieldsAllEventsFromBothWorkflows()
+    {
+        var expectedEvents = Some.Events(howMany: 4).ToList();
+
+        var actualEvents = await Workflow
+            .Start(Some.Workflow.FromEvents(expectedEvents.Take(2)))
+            .Then(
+                createContinuation: (int n, string s, long l) => Some.Workflow.FromEvents(expectedEvents.Skip(2).Take(n)),
+                arg1: 2,
+                arg2: "some second argument",
+                arg3: 42L)
+            .ToListAsync();
+
+        actualEvents.Should().Equal(expectedEvents);
+    }
 }
