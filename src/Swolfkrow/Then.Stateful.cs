89d6c8d3c30eeabbe7ab9c9c0c21daf8a6e187c3
Rubén Campos Zurriaga <ruben.campos.zurriaga@gmail.com>
2023-06-03 22:43:05 +1000

    feat: add stateful continuations with asynchronous folders

--- src/Swolfkrow/Then.Stateful.cs
+++ src/Swolfkrow/Then.Stateful.cs
@@ -19,8 +19,61 @@ public static partial class Workflow
     {
         TState currentState = initialState;
 
+        void UpdateCurrentState(TEvent nextEvent)
+            => currentState = computeNextState(currentState, nextEvent);
+
         return workflow
-            .WithSideEffect(nextEvent => currentState = computeNextState(currentState, nextEvent))
+            .WithSideEffect(UpdateCurrentState)
+            .Then(() => createContinuation(currentState));
+    }
+
+    /// <summary>
+    /// Continues an existing asynchronous workflow with an asynchronous workflow created by a factory that takes a state folded from the yielded events. 
+    /// </summary>
+    /// <param name="workflow">An existing asynchronous workflow.</param>
+    /// <param name="createContinuation">A factory that takes a state folded from all events yielded by the given asynchronous <paramref name="workflow"/> and returns an asynchronous workflow that is to be executed as continuation.</param>
+    /// <param name="computeNextState">An asynchronous folder that computes a next state based on a previous state and an event.</param>
+    /// <param name="initialState">An initial state.</param>
+    /// <typeparam name="TEvent">The (base) type of the events yielded by the asynchronous workflows.</typeparam>
+    /// <typeparam name="TState">The type of the state.</typeparam>
+    /// <returns>An asynchronous workflow composed of the given asynchronous <paramref name="workflow"/>, followed by an asynchronous workflow created from a state computed from its yielded events.</returns>
+    public static IAsyncEnumerable<TEvent> Then<TEvent, TState>(this IAsyncEnumerable<TEvent> workflow,
+        Func<TState, IAsyncEnumerable<TEvent>> createContinuation,
+        Func<TState, TEvent, Task<TState>> computeNextState,
+        TState initialState)
+    {
+        TState currentState = initialState;
+
+        async Task UpdateCurrentState(TEvent nextEvent)
+            => currentState = await computeNextState(currentState, nextEvent);
+
+        return workflow
+            .WithSideEffect(UpdateCurrentState)
+            .Then(() => createContinuation(currentState));
+    }
+
+    /// <summary>
+    /// Continues an existing asynchronous workflow with an asynchronous workflow created by a factory that takes a state folded from the yielded events. 
+    /// </summary>
+    /// <param name="workflow">An existing asynchronous workflow.</param>
+    /// <param name="createContinuation">A factory that takes a state folded from all events yielded by the given asynchronous <paramref name="workflow"/> and returns an asynchronous workflow that is to be executed as continuation.</param>
+    /// <param name="computeNextState">An asynchronous folder that computes a next state based on a previous state and an event.</param>
+    /// <param name="initialState">An initial state.</param>
+    /// <typeparam name="TEvent">The (base) type of the events yielded by the asynchronous workflows.</typeparam>
+    /// <typeparam name="TState">The type of the state.</typeparam>
+    /// <returns>An asynchronous workflow composed of the given asynchronous <paramref name="workflow"/>, followed by an asynchronous workflow created from a state computed from its yielded events.</returns>
+    public static IAsyncEnumerable<TEvent> Then<TEvent, TState>(this IAsyncEnumerable<TEvent> workflow,
+        Func<TState, IAsyncEnumerable<TEvent>> createContinuation,
+        Func<TState, TEvent, ValueTask<TState>> computeNextState,
+        TState initialState)
+    {
+        TState currentState = initialState;
+
+        async ValueTask UpdateCurrentState(TEvent nextEvent)
+            => currentState = await computeNextState(currentState, nextEvent);
+
+        return workflow
+            .WithSideEffect(UpdateCurrentState)
             .Then(() => createContinuation(currentState));
     }
 }
--- tests/Swolfkrow.Tests/ThenTests.cs
+++ tests/Swolfkrow.Tests/ThenTests.cs
@@ -78,21 +78,4 @@ public class ThenTests
 
         actualEvents.Should().Equal(expectedEvents);
     }
-
-    [Test]
-    public async Task StatefulContinuationYieldsAllEvents()
-    {
-        var expectedEvents = Some.Events(howMany: 5).ToList();
-
-        var actualEvents = await Workflow
-            .Start(Some.Workflow.FromEvents(expectedEvents.Take(2)))
-            .Then(
-                createContinuation: (int eventCount) => Some.Workflow.FromEvents(expectedEvents.Skip(eventCount)),
-                computeNextState: (currentState, nextEvent) => currentState + 1,
-                initialState: 0)
-            .ToListAsync();
-
-        actualEvents.Should().Equal(expectedEvents);
- 
-    }
 }
