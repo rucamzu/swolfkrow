648330c92093db9fdbd490fc46704ba89fccea98
Rubén Campos Zurriaga <ruben.campos.zurriaga@gmail.com>
2023-06-09 18:01:00 +1000

    feat: add selective side effects

--- src/Swolfkrow/SideEffect.cs
+++ src/Swolfkrow/SideEffect.cs
@@ -6,9 +6,9 @@ public static partial class Workflow
     /// Injects a synchronous side effect into an asynchronous workflow.
     /// </summary>
     /// <param name="workflow">An asynchronous workflow.</param>
-    /// <param name="effect">An action encapsulating a synchronous side effect that will be executed once per each <typeparamref name="TEvent"/> yielded by the given asynchronous <paramref name="workflow"/>.</param>
+    /// <param name="effect">An action encapsulating a synchronous side effect that takes an <typeparamref name="TEvent"/> argument.</param>
     /// <typeparam name="TEvent">The (base) type of the events yielded by the given asynchronous <paramref name="workflow"/>.</typeparam>
-    /// <returns>An asynchronous workflow that yields each <typeparamref name="TEvent"/> from the given asynchronous <paramref name="workflow"/> after executing the given side <paramref name="effect"/> on them.</returns>
+    /// <returns>An asynchronous workflow that yields each <typeparamref name="TEvent"/> from the given asynchronous <paramref name="workflow"/>, after executing the given side <paramref name="effect"/> on each of them.</returns>
     public static async IAsyncEnumerable<TEvent> WithSideEffect<TEvent>(this IAsyncEnumerable<TEvent> workflow,
         Action<TEvent> effect)
     {
@@ -21,12 +21,34 @@ public static partial class Workflow
     }
 
     /// <summary>
+    /// Injects a synchronous side effect into an asynchronous workflow.
+    /// </summary>
+    /// <param name="workflow">An asynchronous workflow.</param>
+    /// <param name="effect">An action encapsulating a synchronous side effect that takes an <typeparamref name="TEvent"/> argument.</param>
+    /// <param name="predicate">A predicate that takes an <typeparamref name="TEvent"/> argument.</param>
+    /// <typeparam name="TEvent">The (base) type of the events yielded by the given asynchronous <paramref name="workflow"/>.</typeparam>
+    /// <returns>An asynchronous workflow that yields each <typeparamref name="TEvent"/> from the given asynchronous <paramref name="workflow"/>, after executing the given side <paramref name="effect"/> on those events that satisfy the given <paramref name="predicate"/>.</returns>
+    public static async IAsyncEnumerable<TEvent> WithSideEffect<TEvent>(this IAsyncEnumerable<TEvent> workflow,
+        Action<TEvent> effect, Func<TEvent, bool> predicate)
+    {
+        await foreach (var nextEvent in workflow)
+        {
+            if (predicate(nextEvent))
+            {
+                effect(nextEvent);
+            }
+
+            yield return nextEvent;
+        }
+    }
+
+    /// <summary>
     /// Injects an asynchronous side effect into an asynchronous workflow.
     /// </summary>
     /// <param name="workflow">An asynchronous workflow.</param>
     /// <param name="effect">A function encapsulating an asynchronous side effect that will be executed once per each <typeparamref name="TEvent"/> yielded by the given asynchronous <paramref name="workflow"/>.</param>
     /// <typeparam name="TEvent">The (base) type of the events yielded by the given asynchronous <paramref name="workflow"/>.</typeparam>
-    /// <returns>An asynchronous workflow that yields each <typeparamref name="TEvent"/> from the given asynchronous <paramref name="workflow"/> after executing the given side <paramref name="effect"/> on them.</returns>
+    /// <returns>An asynchronous workflow that yields each <typeparamref name="TEvent"/> from the given asynchronous <paramref name="workflow"/>, after executing the given side <paramref name="effect"/> on each of them.</returns>
     public static async IAsyncEnumerable<TEvent> WithSideEffect<TEvent>(this IAsyncEnumerable<TEvent> workflow,
         Func<TEvent, Task> effect)
     {
@@ -44,7 +66,7 @@ public static partial class Workflow
     /// <param name="workflow">An asynchronous workflow.</param>
     /// <param name="effect">A function encapsulating an asynchronous side effect that will be executed once per each <typeparamref name="TEvent"/> yielded by the given asynchronous <paramref name="workflow"/>.</param>
     /// <typeparam name="TEvent">The (base) type of the events yielded by the given asynchronous <paramref name="workflow"/>.</typeparam>
-    /// <returns>An asynchronous workflow that yields each <typeparamref name="TEvent"/> from the given asynchronous <paramref name="workflow"/> after executing the given side <paramref name="effect"/> on them.</returns>
+    /// <returns>An asynchronous workflow that yields each <typeparamref name="TEvent"/> from the given asynchronous <paramref name="workflow"/>, after executing the given side <paramref name="effect"/> on each of them.</returns>
     public static async IAsyncEnumerable<TEvent> WithSideEffect<TEvent>(this IAsyncEnumerable<TEvent> workflow,
         Func<TEvent, ValueTask> effect)
     {
--- tests/Swolfkrow.Tests/SideEffectTests.Action.cs
+++ tests/Swolfkrow.Tests/SideEffectTests.Action.cs
@@ -12,8 +12,7 @@ public partial class SideEffect
         var expectedEvents = Some.Events(howMany: 3).ToList();
         var effectedEvents = new List<Some.Event>();
 
-        void SideEffect(Some.Event nextEvent)
-            => effectedEvents!.Add(nextEvent);
+        void SideEffect(Some.Event nextEvent) => effectedEvents!.Add(nextEvent);
 
         var actualEvents = await Workflow
             .Start(Some.Workflow.FromEvents(expectedEvents))
@@ -29,12 +28,45 @@ public partial class SideEffect
         var expectedEvents = Some.Events(howMany: 3).ToList();
         var effectedEvents = new List<Some.Event>();
 
-        void SideEffect(Some.Event nextEvent)
-            => effectedEvents!.Add(nextEvent);
+        void SideEffect(Some.Event nextEvent) => effectedEvents!.Add(nextEvent);
 
         var actualEvents = await Workflow
             .Start(Some.Workflow.FromEvents(expectedEvents))
             .WithSideEffect(SideEffect)
+            .ToListAsync();
+
+        expectedEvents.Should().Equal(expectedEvents);
+    }
+
+    [Test]
+    public async Task FromActionWithPredicateExecutesOnlyOncePerEventSatisfyingPredicate()
+    {
+        var expectedEvents = Some.Events(howMany: 3).ToList();
+        var effectedEvents = new List<Some.Event>();
+
+        void SideEffect(Some.Event nextEvent) => effectedEvents!.Add(nextEvent);
+        bool Predicate(Some.Event nextEvent) => !nextEvent.Description.Contains("2");
+
+        var actualEvents = await Workflow
+            .Start(Some.Workflow.FromEvents(expectedEvents))
+            .WithSideEffect(SideEffect, Predicate)
+            .ToListAsync();
+
+        effectedEvents.Should().Equal(expectedEvents.Where(Predicate));
+    }
+
+    [Test]
+    public async Task FromActionWithPredicatePreservesEvents()
+    {
+        var expectedEvents = Some.Events(howMany: 3).ToList();
+        var effectedEvents = new List<Some.Event>();
+
+        void SideEffect(Some.Event nextEvent) => effectedEvents!.Add(nextEvent);
+        bool Predicate(Some.Event nextEvent) => !nextEvent.Description.Contains("2");
+
+        var actualEvents = await Workflow
+            .Start(Some.Workflow.FromEvents(expectedEvents))
+            .WithSideEffect(SideEffect, Predicate)
             .ToListAsync();
 
         expectedEvents.Should().Equal(expectedEvents);
