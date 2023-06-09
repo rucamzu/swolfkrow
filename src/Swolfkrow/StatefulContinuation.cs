3a9dd1c80800293ad62083431d90ecd87f9eeb55
Rubén Campos Zurriaga <ruben.campos.zurriaga@gmail.com>
2023-06-10 00:32:10 +1000

    docs: improve comments consistency

--- src/Swolfkrow/StatefulContinuation.cs
+++ src/Swolfkrow/StatefulContinuation.cs
@@ -3,15 +3,15 @@
 public static partial class Workflow
 {
     /// <summary>
-    /// Continues an existing asynchronous workflow with an asynchronous workflow factory that takes a state folded from the yielded events. 
+    /// Continues an asynchronous workflow with an asynchronous workflow factory that takes a state folded from the yielded events. 
     /// </summary>
-    /// <param name="workflow">An existing asynchronous workflow.</param>
-    /// <param name="createContinuation">A factory that takes a state folded from all events yielded by the given asynchronous <paramref name="workflow"/> and returns an asynchronous workflow that is to be executed as continuation.</param>
-    /// <param name="computeNextState">A folder that computes a next state based on a previous state and an event.</param>
-    /// <param name="initialState">An initial state.</param>
-    /// <typeparam name="TEvent">The (base) type of the events yielded by the asynchronous workflows.</typeparam>
-    /// <typeparam name="TState">The type of the state.</typeparam>
-    /// <returns>An asynchronous workflow composed of the given asynchronous <paramref name="workflow"/>, followed by an asynchronous workflow created from a state computed from its yielded events.</returns>
+    /// <param name="workflow">An asynchronous workflow.</param>
+    /// <param name="createContinuation">A factory that takes a <typeparamref name="TState"/> and returns an asynchronous workflow.</param>
+    /// <param name="computeNextState">A folder that takes a previous <typeparamref name="TState"/> and an <typeparamref name="TEvent"/> and returns a next <typeparamref name="TState"/>.</param>
+    /// <param name="initialState">An initial <typeparamref name="TState"/>.</param>
+    /// <typeparam name="TEvent">The (base) type of the events yielded by the asynchronous <paramref name="workflow"/>.</typeparam>
+    /// <typeparam name="TState">The type of the folded state.</typeparam>
+    /// <returns>An asynchronous workflow that yields the events yielded by the given asynchronous <paramref name="workflow"/>, followed by the events yielded by the asynchronous workflow returned by calling the given <paramref name="createContinuation"/> factory with the state computed by the given <paramref name="computeNextState"/> folder.</returns>
     public static IAsyncEnumerable<TEvent> Then<TEvent, TState>(this IAsyncEnumerable<TEvent> workflow,
         Func<TState, IAsyncEnumerable<TEvent>> createContinuation,
         Func<TState, TEvent, TState> computeNextState,
@@ -28,15 +28,15 @@ public static partial class Workflow
     }
 
     /// <summary>
-    /// Continues an existing asynchronous workflow with an asynchronous workflow factory that takes a state folded from the yielded events. 
+    /// Continues an asynchronous workflow with an asynchronous workflow factory that takes a state folded from the yielded events. 
     /// </summary>
-    /// <param name="workflow">An existing asynchronous workflow.</param>
-    /// <param name="createContinuation">A factory that takes a state folded from all events yielded by the given asynchronous <paramref name="workflow"/> and returns an asynchronous workflow that is to be executed as continuation.</param>
-    /// <param name="computeNextState">An asynchronous folder that computes a next state based on a previous state and an event.</param>
-    /// <param name="initialState">An initial state.</param>
-    /// <typeparam name="TEvent">The (base) type of the events yielded by the asynchronous workflows.</typeparam>
-    /// <typeparam name="TState">The type of the state.</typeparam>
-    /// <returns>An asynchronous workflow composed of the given asynchronous <paramref name="workflow"/>, followed by an asynchronous workflow created from a state computed from its yielded events.</returns>
+    /// <param name="workflow">An asynchronous workflow.</param>
+    /// <param name="createContinuation">A factory that takes a <typeparamref name="TState"/> and returns an asynchronous workflow.</param>
+    /// <param name="computeNextState">An asynchronous folder that takes a previous <typeparamref name="TState"/> and an <typeparamref name="TEvent"/> and returns a next <typeparamref name="TState"/>.</param>
+    /// <param name="initialState">An initial <typeparamref name="TState"/>.</param>
+    /// <typeparam name="TEvent">The (base) type of the events yielded by the asynchronous <paramref name="workflow"/>.</typeparam>
+    /// <typeparam name="TState">The type of the folded state.</typeparam>
+    /// <returns>An asynchronous workflow that yields the events yielded by the given asynchronous <paramref name="workflow"/>, followed by the events yielded by the asynchronous workflow returned by awaiting the task returned by calling the given <paramref name="createContinuation"/> factory with the state computed by the given <paramref name="computeNextState"/> folder.</returns>
     public static IAsyncEnumerable<TEvent> Then<TEvent, TState>(this IAsyncEnumerable<TEvent> workflow,
         Func<TState, IAsyncEnumerable<TEvent>> createContinuation,
         Func<TState, TEvent, Task<TState>> computeNextState,
@@ -53,15 +53,15 @@ public static partial class Workflow
     }
 
     /// <summary>
-    /// Continues an existing asynchronous workflow with an asynchronous workflow factory that takes a state folded from the yielded events. 
+    /// Continues an asynchronous workflow with an asynchronous workflow factory that takes a state folded from the yielded events. 
     /// </summary>
-    /// <param name="workflow">An existing asynchronous workflow.</param>
-    /// <param name="createContinuation">A factory that takes a state folded from all events yielded by the given asynchronous <paramref name="workflow"/> and returns an asynchronous workflow that is to be executed as continuation.</param>
-    /// <param name="computeNextState">An asynchronous folder that computes a next state based on a previous state and an event.</param>
-    /// <param name="initialState">An initial state.</param>
-    /// <typeparam name="TEvent">The (base) type of the events yielded by the asynchronous workflows.</typeparam>
-    /// <typeparam name="TState">The type of the state.</typeparam>
-    /// <returns>An asynchronous workflow composed of the given asynchronous <paramref name="workflow"/>, followed by an asynchronous workflow created from a state computed from its yielded events.</returns>
+    /// <param name="workflow">An asynchronous workflow.</param>
+    /// <param name="createContinuation">A factory that takes a <typeparamref name="TState"/> and returns an asynchronous workflow.</param>
+    /// <param name="computeNextState">An asynchronous folder that takes a previous <typeparamref name="TState"/> and an <typeparamref name="TEvent"/> and returns a next <typeparamref name="TState"/>.</param>
+    /// <param name="initialState">An initial <typeparamref name="TState"/>.</param>
+    /// <typeparam name="TEvent">The (base) type of the events yielded by the asynchronous <paramref name="workflow"/>.</typeparam>
+    /// <typeparam name="TState">The type of the folded state.</typeparam>
+    /// <returns>An asynchronous workflow that yields the events yielded by the given asynchronous <paramref name="workflow"/>, followed by the events yielded by the asynchronous workflow returned by awaiting the task returned by calling the given <paramref name="createContinuation"/> factory with the state computed by the given <paramref name="computeNextState"/> folder.</returns>
     public static IAsyncEnumerable<TEvent> Then<TEvent, TState>(this IAsyncEnumerable<TEvent> workflow,
         Func<TState, IAsyncEnumerable<TEvent>> createContinuation,
         Func<TState, TEvent, ValueTask<TState>> computeNextState,
