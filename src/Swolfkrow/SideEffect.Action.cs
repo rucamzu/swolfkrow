7779b4b126c1b4d2137a03855f9a3bf48f8ddf0c
Rubén Campos Zurriaga <ruben.campos.zurriaga@gmail.com>
2023-06-09 23:04:52 +1000

    refactor: split side effects into separate files

--- src/Swolfkrow/SideEffect.Action.cs
+++ src/Swolfkrow/SideEffect.Action.cs
@@ -41,40 +41,4 @@ public static partial class Workflow
             yield return nextEvent;
         }
     }
-
-    /// <summary>
-    /// Injects an asynchronous side effect into an asynchronous workflow.
-    /// </summary>
-    /// <param name="workflow">An asynchronous workflow.</param>
-    /// <param name="effect">A function encapsulating an asynchronous side effect that will be executed once per each <typeparamref name="TEvent"/> yielded by the given asynchronous <paramref name="workflow"/>.</param>
-    /// <typeparam name="TEvent">The (base) type of the events yielded by the given asynchronous <paramref name="workflow"/>.</typeparam>
-    /// <returns>An asynchronous workflow that yields each <typeparamref name="TEvent"/> from the given asynchronous <paramref name="workflow"/>, after executing the given side <paramref name="effect"/> on each of them.</returns>
-    public static async IAsyncEnumerable<TEvent> WithSideEffect<TEvent>(this IAsyncEnumerable<TEvent> workflow,
-        Func<TEvent, Task> effect)
-    {
-        await foreach (var nextEvent in workflow)
-        {
-            await effect(nextEvent);
-
-            yield return nextEvent;
-        }
-    }
-
-    /// <summary>
-    /// Injects an asynchronous side effect into an asynchronous workflow.
-    /// </summary>
-    /// <param name="workflow">An asynchronous workflow.</param>
-    /// <param name="effect">A function encapsulating an asynchronous side effect that will be executed once per each <typeparamref name="TEvent"/> yielded by the given asynchronous <paramref name="workflow"/>.</param>
-    /// <typeparam name="TEvent">The (base) type of the events yielded by the given asynchronous <paramref name="workflow"/>.</typeparam>
-    /// <returns>An asynchronous workflow that yields each <typeparamref name="TEvent"/> from the given asynchronous <paramref name="workflow"/>, after executing the given side <paramref name="effect"/> on each of them.</returns>
-    public static async IAsyncEnumerable<TEvent> WithSideEffect<TEvent>(this IAsyncEnumerable<TEvent> workflow,
-        Func<TEvent, ValueTask> effect)
-    {
-        await foreach (var nextEvent in workflow)
-        {
-            await effect(nextEvent);
-
-            yield return nextEvent;
-        }
-    }
 }
