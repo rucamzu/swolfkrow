04b4fe96aa0dfc7b351395806c264a68b9b91b80
Rubén Campos Zurriaga <ruben.campos.zurriaga@gmail.com>
2023-06-10 00:41:11 +1000

    refactor: refactor stateful continuation tests

--- tests/Swolfkrow.Tests/StatefulContinuationTests.cs
+++ tests/Swolfkrow.Tests/StatefulContinuationTests.cs
@@ -3,57 +3,61 @@ using NUnit.Framework;
 
 namespace Swolfkrow.Tests;
 
-public class StatefulContinuationTests
+[TestFixture]
+public partial class StatefulContinuation
 {
     [Test]
-    public async Task StatefulContinuationYieldsAllEvents()
+    public async Task FromEventAsyncEnumerableFactoryAndFolderYieldsAllEvents()
     {
         var expectedEvents = Some.Events(howMany: 5).ToList();
 
+        IAsyncEnumerable<Some.Event> EventAsyncEnumerableFactory(int eventCount)
+            => Some.Workflow.FromEvents(expectedEvents!.Skip(eventCount));
+
+        int Folder(int currentState, Some.Event nextEvent)
+            => currentState + 1;
+
         var actualEvents = await Workflow
             .Start(Some.Workflow.FromEvents(expectedEvents.Take(2)))
-            .Then(
-                createContinuation: (int eventCount) => Some.Workflow.FromEvents(expectedEvents.Skip(eventCount)),
-                computeNextState: (currentState, nextEvent) => currentState + 1,
-                initialState: 0)
+            .Then(EventAsyncEnumerableFactory, Folder, initialState: 0)
             .ToListAsync();
 
         actualEvents.Should().Equal(expectedEvents);
     }
 
     [Test]
-    public async Task StatefulContinuationWithTaskFolderYieldsAllEvents()
+    public async Task FromEventAsyncEnumerableFactoryAndTaskFolderYieldsAllEvents()
     {
         var expectedEvents = Some.Events(howMany: 5).ToList();
 
-        async Task<int> IncrementState(int currentState, Some.Event _)
+        IAsyncEnumerable<Some.Event> EventAsyncEnumerableFactory(int eventCount)
+            => Some.Workflow.FromEvents(expectedEvents!.Skip(eventCount));
+
+        async Task<int> Folder(int currentState, Some.Event nextEvent)
             => await Task.FromResult(currentState + 1);
 
         var actualEvents = await Workflow
             .Start(Some.Workflow.FromEvents(expectedEvents.Take(2)))
-            .Then(
-                createContinuation: (int eventCount) => Some.Workflow.FromEvents(expectedEvents.Skip(eventCount)),
-                computeNextState: IncrementState,
-                initialState: 0)
+            .Then(EventAsyncEnumerableFactory, Folder, initialState: 0)
             .ToListAsync();
 
         actualEvents.Should().Equal(expectedEvents);
     }
 
     [Test]
-    public async Task StatefulContinuationWithValueTaskFolderYieldsAllEvents()
+    public async Task FromEventAsyncEnumerableFactoryAndValueTaskFolderYieldsAllEvents()
     {
         var expectedEvents = Some.Events(howMany: 5).ToList();
 
-        async ValueTask<int> IncrementState(int currentState, Some.Event _)
+        IAsyncEnumerable<Some.Event> EventAsyncEnumerableFactory(int eventCount)
+            => Some.Workflow.FromEvents(expectedEvents!.Skip(eventCount));
+
+        async ValueTask<int> Folder(int currentState, Some.Event nextEvent)
             => await ValueTask.FromResult(currentState + 1);
 
         var actualEvents = await Workflow
             .Start(Some.Workflow.FromEvents(expectedEvents.Take(2)))
-            .Then(
-                createContinuation: (int eventCount) => Some.Workflow.FromEvents(expectedEvents.Skip(eventCount)),
-                computeNextState: IncrementState,
-                initialState: 0)
+            .Then(EventAsyncEnumerableFactory, Folder, initialState: 0)
             .ToListAsync();
 
         actualEvents.Should().Equal(expectedEvents);
