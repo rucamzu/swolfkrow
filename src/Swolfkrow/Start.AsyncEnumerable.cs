aade0012bcfbffba5f08bd1c720157cb08ca501a
Rubén Campos Zurriaga <ruben.campos.zurriaga@gmail.com>
2023-06-10 00:58:03 +1000

    docs: fix comment

--- src/Swolfkrow/Start.AsyncEnumerable.cs
+++ src/Swolfkrow/Start.AsyncEnumerable.cs
@@ -1,7 +1,7 @@
 ﻿namespace Swolfkrow;
 
 /// <summary>
-/// Factory methods and extension methods to compose asynchronous workflows based on <see cref="IAsyncEnumerable{T}"/>.
+/// Factory methods to compose asynchronous workflows based on <see cref="IAsyncEnumerable{T}"/>.
 /// </summary>
 public static partial class Workflow
 {
