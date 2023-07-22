2842031bf5b172972068ae7ec01ef35d4c78539a
Rubén Campos Zurriaga <ruben.campos.zurriaga@gmail.com>
2023-07-22 19:00:28 +1000

    feat: add While overloads with additional arguments

--- src/Swolfkrow/Workflow.While.Func.cs
+++ src/Swolfkrow/Workflow.While.Func.cs
@@ -9,6 +9,46 @@ public partial class Workflow<TEvent>
     /// </summary>
     /// <param name="predicate">A predicate that takes an <typeparamref name="TEvent"/>.</param>
     /// <returns>A new asynchronous <see cref="Workflow{TEvent}"/> that yields the events yielded by <see langword="this"/> asynchronous workflow up to, but excluding, the first event that does not satisfy the given <paramref name="predicate"/>.</returns>
-    public Workflow<TEvent> While(Func<TEvent, bool> predicate)
+    public Workflow<TEvent> While(
+        Func<TEvent, bool> predicate)
         => WorkflowImpl.WhileFromPredicate(_workflow, predicate).ToWorkflow();
-}   
+
+    /// <summary>
+    /// Interupts the asynchronous <see cref="Workflow{TEvent}"/> right before the first event that does not satisfy a predicate.
+    /// </summary>
+    /// <param name="predicate">A predicate that takes an <typeparamref name="TEvent"/> and one additional argument.</param>
+    /// <param name="arg">The argument passed to the given <paramref name="predicate"/>.</param>
+    /// <typeparam name="TArg">The type of the argument passed to the given <paramref name="predicate"/>.</typeparam>
+    /// <returns>A new asynchronous <see cref="Workflow{TEvent}"/> that yields the events yielded by <see langword="this"/> asynchronous workflow up to, but excluding, the first event that does not satisfy the given <paramref name="predicate"/>.</returns>
+    public Workflow<TEvent> While<TArg>(
+        Func<TEvent, TArg, bool> predicate, TArg arg)
+        => WorkflowImpl.WhileFromPredicate(_workflow, predicate.BindLast(arg)).ToWorkflow();
+
+    /// <summary>
+    /// Interupts the asynchronous <see cref="Workflow{TEvent}"/> right before the first event that does not satisfy a predicate.
+    /// </summary>
+    /// <param name="predicate">A predicate that takes an <typeparamref name="TEvent"/> and one additional argument.</param>
+    /// <param name="arg1">The first argument passed to the given <paramref name="predicate"/>.</param>
+    /// <param name="arg2">The second argument passed to the given <paramref name="predicate"/>.</param>
+    /// <typeparam name="TArg1">The type of the first argument passed to the given <paramref name="predicate"/>.</typeparam>
+    /// <typeparam name="TArg2">The type of the second argument passed to the given <paramref name="predicate"/>.</typeparam>
+    /// <returns>A new asynchronous <see cref="Workflow{TEvent}"/> that yields the events yielded by <see langword="this"/> asynchronous workflow up to, but excluding, the first event that does not satisfy the given <paramref name="predicate"/>.</returns>
+    public Workflow<TEvent> While<TArg1, TArg2>(
+        Func<TEvent, TArg1, TArg2, bool> predicate, TArg1 arg1, TArg2 arg2)
+        => WorkflowImpl.WhileFromPredicate(_workflow, predicate.BindLast(arg1, arg2)).ToWorkflow();
+
+    /// <summary>
+    /// Interupts the asynchronous <see cref="Workflow{TEvent}"/> right before the first event that does not satisfy a predicate.
+    /// </summary>
+    /// <param name="predicate">A predicate that takes an <typeparamref name="TEvent"/> and one additional argument.</param>
+    /// <param name="arg1">The first argument passed to the given <paramref name="predicate"/>.</param>
+    /// <param name="arg2">The second argument passed to the given <paramref name="predicate"/>.</param>
+    /// <param name="arg3">The third argument passed to the given <paramref name="predicate"/>.</param>
+    /// <typeparam name="TArg1">The type of the first argument passed to the given <paramref name="predicate"/>.</typeparam>
+    /// <typeparam name="TArg2">The type of the second argument passed to the given <paramref name="predicate"/>.</typeparam>
+    /// <typeparam name="TArg3">The type of the third argument passed to the given <paramref name="predicate"/>.</typeparam>
+    /// <returns>A new asynchronous <see cref="Workflow{TEvent}"/> that yields the events yielded by <see langword="this"/> asynchronous workflow up to, but excluding, the first event that does not satisfy the given <paramref name="predicate"/>.</returns>
+    public Workflow<TEvent> While<TArg1, TArg2, TArg3>(
+        Func<TEvent, TArg1, TArg2, TArg3, bool> predicate, TArg1 arg1, TArg2 arg2, TArg3 arg3)
+        => WorkflowImpl.WhileFromPredicate(_workflow, predicate.BindLast(arg1, arg2, arg3)).ToWorkflow();
+}
--- tests/Swolfkrow.Tests/WorkflowTests.While.ValueTask.cs
+++ tests/Swolfkrow.Tests/WorkflowTests.While.ValueTask.cs
@@ -22,4 +22,52 @@ public partial class While
 
         actualEvents.Should().Equal(expectedEvents.Take(2));
     }
+
+    [Test]
+    public async Task YieldsOnlyEventsThatSatisfyValueTaskPredicateWithOneAdditionalArgument()
+    {
+        var expectedEvents = Some.Events(howMany: 4).ToList();
+
+        async ValueTask<bool> Predicate(Some.Event nextEvent, int arg1)
+            => await new ValueTask<bool>(!nextEvent.Description.Contains("3"));
+
+        var actualEvents = await Workflow
+            .Start(Some.Events(howMany: 4))
+            .While(Predicate, 42)
+            .ToListAsync();
+
+        actualEvents.Should().Equal(expectedEvents.Take(2));
+    }
+
+    [Test]
+    public async Task YieldsOnlyEventsThatSatisfyValueTaskPredicateWithTwoAdditionalArguments()
+    {
+        var expectedEvents = Some.Events(howMany: 4).ToList();
+
+        async ValueTask<bool> Predicate(Some.Event nextEvent, int arg1, string arg2)
+            => await new ValueTask<bool>(!nextEvent.Description.Contains("3"));
+
+        var actualEvents = await Workflow
+            .Start(Some.Events(howMany: 4))
+            .While(Predicate, 42, "42")
+            .ToListAsync();
+
+        actualEvents.Should().Equal(expectedEvents.Take(2));
+    }
+
+    [Test]
+    public async Task YieldsOnlyEventsThatSatisfyValueTaskPredicateWithThreeAdditionalArguments()
+    {
+        var expectedEvents = Some.Events(howMany: 4).ToList();
+
+        async ValueTask<bool> Predicate(Some.Event nextEvent, int arg1, string arg2, long arg3)
+            => await new ValueTask<bool>(!nextEvent.Description.Contains("3"));
+
+        var actualEvents = await Workflow
+            .Start(Some.Events(howMany: 4))
+            .While(Predicate, 42, "42", 42L)
+            .ToListAsync();
+
+        actualEvents.Should().Equal(expectedEvents.Take(2));
+    }
 }
